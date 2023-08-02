7e103c6f817c696f3f49c457929a51dbe0415197
Gabriel Gotlib <gagotlib@gmail.com>
2023-08-02 16:55:08 +0200

    modulo 7 ejercicio 1

---
+++ Clases/Clases M06.js
@@ -0,0 +1,84 @@
+//OBJETOS --> Diccionarios {}
+
+var deportes = {
+    conBalon: ['Futbol', 'Basketball', 'Golf'],
+    sinBalon: ['Boxeo', 'Surf', 'Trekking'],
+ };
+ var persona = { nombre: 'Lucas', edad: 26, estudios: { esProgramador: true } };
+
+ // ACCEDER con . -> objeto.clave
+var persona = { nombre: 'Lucas', edad: 26, estudios: { esProgramador: true } };
+console.log(persona.edad);
+
+// ASIGNAR -> objeto.clave = asignar nuevovalor
+var persona = { nombre: 'Lucas', edad: 26, estudios: { esProgramador: true } };
+persona.nombre = 'Martín';
+console.log(persona.nombre);
+
+//CREAR
+var autos = {};
+autos.marcas = ['Ford', 'Audi', 'Ferrari'];
+console.log(autos);
+
+//BORRAR
+delete autos.marcas;
+console.log(autos);
+
+// DOT NOTATION
+var atuendos = { manos: ['Guantes', 'Anillos'], pies: ['Zapatos', 'Soquetes'] };
+
+// BRACKET NOTATION
+atuendos['piernas'] = ['Bermudas', 'Pantalones'];
+console.log(atuendos);
+
+// HAS OWN PROPERTY
+var libro = { autor: 'Borges', genero: 'Policial', año: 1990 };
+var tienePropiedad = libro.hasOwnProperty('nombre');
+
+console.log(tienePropiedad);
+
+// KEYS
+var libro = { autor: 'Borges', genero: 'Policial', año: 1990 };
+var todasLasPropiedades = Object.keys(libro);
+
+console.log(todasLasPropiedades);
+
+// HAS OWN PROPERTY
+var libro = { autor: 'Borges', genero: 'Policial', año: 1990 };
+var tienePropiedad = libro.hasOwnProperty('nombre');
+
+console.log(tienePropiedad);
+
+
+// KEYS
+var libro = { autor: 'Borges', genero: 'Policial', año: 1990 };
+var todasLasPropiedades = Object.keys(libro);
+
+console.log(todasLasPropiedades);
+
+
+for (let prop in mundo) {
+   console.log('Esta es la propiedad: ', prop);
+   console.log('Este es el valor: ', mundo[prop]);
+}
+
+atuendos = {
+    manos : ['Guantes', 'anillos'],
+    pies : ['zapatillas', 'medias'],
+    cabeza: 'sombrero'
+}
+for ( var cuerpo in atuendos) {
+    console.log('esta es la parte del cuerpo:' + cuerpo)
+    console.log('esta ropa: ' + atuendos[cuerpo])
+}
+
+//THIS
+var mascota = {
+    animal: 'Perro',
+    raza: 'Ovejero Alemán',
+    amistoso: true,
+    dueño: 'María López',
+    info: () => {
+       console.log('Mi perro es un  ' + this.raza);
+    },
+ };
